8a0690b1fd9e3f19ac8d5602652870af8335e6a3
Maxim Smirnov <smirnov.cpp@gmail.com>
2015-08-22 00:44:54 +0300

    added localization script

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 cd
+
 apt-get install unzip gcc python3-pip i2c-tools libi2c-dev python3-dev libjpeg-dev libtiff5 libtiff5-dev libfreetype6 libfreetype6-dev i2c-tools
 pip-3.2 install pillow
+
 git clone https://github.com/madler/zlib.git
 cd zlib
 ./configure
@@ -10,7 +12,12 @@ make
 make install
 cd
 rm -r zlib
+
 cd intel_edison_oled/py-smbus
 python3 setup.py install
 cd ..
 rm -r py-smbus
+
+chmod +x locale_RU.sh
+./locale_RU.sh
+rm locale_RU.sh
---
+++ locale_RU.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+echo "ru_RU.UTF-8 UTF-8" >> /etc/locale.gen
+
+./locale-gen
+
+echo "LC_CTYPE=ru_RU.UTF-8" >> ~/.bashrc
+echo "LC_COLLATE=ru_RU.UTF-8" >> ~/.bashrc
+echo "LANG=ru_RU.UTF-8" >> ~/.bashrc
+echo "export LC_CTYPE LC_COLLATE" >> ~/.bashrc
