224836b44d5307bc8fe10e64bc7dee5fb2828805
Maxim Smirnov <smirnov.cpp@gmail.com>
2015-08-22 10:12:04 +0300

    fixed localization script

--- locale_RU.sh
+++ locale_RU.sh
@@ -3,7 +3,7 @@
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 echo "ru_RU.UTF-8 UTF-8" >> /etc/locale.gen
 
-./locale-gen
+locale-gen
 
 echo "LC_CTYPE=ru_RU.UTF-8" >> ~/.bashrc
 echo "LC_COLLATE=ru_RU.UTF-8" >> ~/.bashrc
